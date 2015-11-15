57f2d862c2f995920256af026287fce2969b5fdc
Safak <s.safak.topcu@gmail.com>
2015-11-16 00:52:33 +0200

    implemented tournament selection

--- solve.py
+++ solve.py
@@ -22,9 +22,9 @@ class Population:
             indiv.gene = self.create_random_gene(length)
             self.individuals.append(indiv)
 
-    def print(self,items,capacity):
+    def print(self, items, capacity):
         for i in self.individuals:
-            print(i.gene )
+            print(i.gene)
             print(fitness(i.gene, items, capacity))
 
 
@@ -36,8 +36,12 @@ def solve_knap_sack(capacity, items):
     # calculate fitness values
     for i in population.individuals:
         i.fitness = fitness(i.gene, items, capacity)
-
-    # while !terminate
+    counter = 0
+    maxiteration = 1
+    while counter != maxiteration:
+        counter += 1
+        tournament_selection(population, 10, 1, 1)
+        # while !terminate
         # selection
         # recombination
         # mutation
@@ -56,7 +60,7 @@ def get_random_gene(length):
 def fitness(gene, items, capacity):
     totvalue = 0
     totweight = 0
-    penalty = 0.2
+    penalty = 0
     i = 0
     for g in gene:
         if g == 1:
@@ -70,3 +74,32 @@ def fitness(gene, items, capacity):
     else:  # feasible
         f = totvalue
     return f
+
+
+def tournament_selection(evaluatedpopulation, tournamentsize, elitesize, winnersize):
+    selection = []
+    while selection.__len__() != evaluatedpopulation.individuals.__len__():
+        tournamenters = select_n_random_from_list(evaluatedpopulation.individuals, tournamentsize)
+        winners = get_n_winners_of_tournament(tournamenters, winnersize)
+        selection.append(winners)
+
+    # add elite
+    return
+
+
+def select_n_random_from_list(individuals, n):
+    i = 0;
+    selection = []
+    while i != n:
+        i += 1
+        selection.append(random.choice(individuals))
+    return selection
+
+def get_n_winners_of_tournament(tournamenters, n):
+    tournamenters = sorted(tournamenters, key=lambda individual: individual.fitness, reverse=True)
+    winners = []
+    i = 0
+    while winners.__len__() != n:
+        winners.append(tournamenters[i])
+        i += 1
+    return winners
